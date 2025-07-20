dad433ad174f990265e2552964ea4feaf84fd74b
Jerryomwesh <jerryomwesh@gmail.com>
2025-07-20 21:06:07 +0300

    Create basic App component with simple styling

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="app-container">
+      <header>
+        <h1>Smart Goal Planner</h1>
+        <p>Track your financial goals and savings progress</p>
+      </header>
+      <main>
+        <p>Goals will be displayed here</p>
+      </main>
+    </div>
+  )
+}
+
+export default App
