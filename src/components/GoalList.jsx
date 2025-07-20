f4bd74f2ee3c6531239e628928c76e36bcc13981
Jerryomwesh <jerryomwesh@gmail.com>
2025-07-20 21:07:21 +0300

    Create GoalList component and integrate it into App

---
+++ src/components/GoalList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function GoalList() {
+  // This is a placeholder for now
+  return (
+    <div className="goal-list">
+      <h2>My Savings Goals</h2>
+      <p>No goals available yet</p>
+    </div>
+  );
+}
+
+export default GoalList;
